7bc926e294b0056e8eee7a8f20f2933991283aab
151250128 <151250128@smail.nju.edu.cn>
2018-06-06 10:52:29 +0800

    <fix>:<change international css styles>

--- src/components/International/International.vue
+++ src/components/International/International.vue
@@ -14,34 +14,34 @@
         <table class="international-list-table">
           <thead>
           <tr>
-            <th></th>
-            <th>比赛时间</th>
-            <th>比赛地点</th>
+            <th class="no-decoration"></th>
+            <th class="no-decoration">比赛时间</th>
+            <th class="no-decoration">比赛地点</th>
           </tr>
           </thead>
           <tbody>
           <tr @click="goToEuropean2018">
-            <td>2018欧洲赛2</td>
+            <th>2018欧洲赛2</th>
             <td>预计下半年</td>
             <td>维也纳</td>
           </tr>
           <tr @click="goToSAmerican2018">
-            <td>2018南美赛</td>
+            <th>2018南美赛</th>
             <td>预计9月</td>
             <td>巴西</td>
           </tr>
           <tr @click="goToEuropean2018">
-            <td>2018欧洲赛1</td>
+            <th>2018欧洲赛1</th>
             <td>2018年7月16日</td>
             <td>里斯本</td>
           </tr>
           <tr @click="goToAmerican2018">
-            <td>2018北美赛</td>
+            <th>2018北美赛</th>
             <td>2018年1月27日</td>
             <td>达拉斯</td>
           </tr>
           <tr @click="goToEuropean2017">
-            <td>2017欧洲赛</td>
+            <th>2017欧洲赛</th>
             <td>2017年7月25日</td>
             <td>布拉格</td>
           </tr>
